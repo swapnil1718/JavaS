91517f08e259b9b7861f328275810350b18e88e4
Swapnil Jadhav <Swapniljadhav0914@gmail.com>
2025-07-31 07:14:54 +0000

    ConversionOperation

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,36 @@
+let score="1875abc";
+//console.log(typeof(score));
+ let valueNumber=Number(score);
+ //console.log(typeof(valueNumber));
+
+ //console.log(valueNumber);
+ //console.log(valueNumber);
+
+
+ /*
+
+"33"==>33
+"33abc"===>NaN
+true===>1 && false===>0
+
+ */
+
+let isLoggedIn=1;
+let booleanLoggnedIn=Boolean(isLoggedIn);
+
+//console.log(booleanLoggnedIn);//==>it convert the 1 into the true value. and also convert the 0 value into false.
+
+
+
+let booleanIs="swapnil";
+let isBoolean=Boolean(booleanIs);
+//console.log(isBoolean);
+
+//suppose we convert ""==>false
+//suppose we convert "swapnil"==>true
+
+
+let isNumber=1718;
+let isString=String(isNumber);
+console.log(isString);
+console.log(typeof(isString));
